2c7dccbc63fa723587f849def5725b488a826d80
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-10-28 18:49:37 +0100

    Add files via upload

--- HydroAccept.py
+++ HydroAccept.py
@@ -14,12 +14,12 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    role = discord.utils.get(member.guild.roles, name='Members')
+    role = discord.utils.get(member.guild.roles, name='Applicants')
     welcome = client.get_channel(621326188555796481)
     applications = client.get_channel(494184460275941377)
     info = '<#584777370557349898>'
     await member.add_roles(role)
-    await welcome.send(f'Welcome {member.mention} to **Hydro Vanilla SMP!** :hydrosmp:')
+    await welcome.send(f'Welcome {member.mention} to **Hydro Vanilla SMP!** <:hydrosmp:579754809272434695>')
     await applications.send(f'Welcome {member.mention} to **Hydro Vanilla SMP** <:hydrosmp:579754809272434695>! \n**Make sure to read** {info} **to check on how to Apply!**')
     await applications.send('```IGN?:\nLocation?:\nWhat year were you born?:\n\nHow long have you been playing Minecraft SMP?:\nWhat do you primarily do while playing Minecraft?:\nHow many past servers have you played on?:\nWill you be active in Discord and do you want to participate in group events?:\nHow much time can you play a week? (estimated):\nBriefly explain your plans for this server:\n\nWhere did you find out about Hydro SMP? (If it is a website, post the link):\nIf it was on Reddit, what is your Reddit username?```')
     await applications.send('Copy this template ^ and post it in this channel when you are done filling it in! :white_check_mark:')
