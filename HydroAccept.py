9ed8b7bd1d5afcec6af7a8ec8ec5ae4052b6c58a
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-10-29 13:39:37 +0100

    Add files via upload

--- HydroAccept.py
+++ HydroAccept.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix = '.', intents=intents)
+client = commands.Bot(command_prefix = '.', intents=intents, help_command=None)
 
 
 @client.event
