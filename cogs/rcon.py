d82231e4a27910c3a2a900173ae9259e9e03bce0
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-01 18:25:38 +0100

    Add files via upload

--- cogs/rcon.py
+++ cogs/rcon.py
@@ -106,6 +106,7 @@ class RCON(commands.Cog):
                 await ctx.guild.unban(user)
                 await ctx.send(f'Unbanned {user.mention}')
                 return
+            print(user.name)
 
     @unban.error
     async def unban_error(self, member, error):
