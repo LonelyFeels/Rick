16e1a24094e7543177447e25727afd7204dd7987
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-01 13:05:25 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-token = 'NzcwNzM5MTkwNDYzMDcwMjIy.X5h8zg.RNSam5Ri0ae1dS1WVllUrysn_AA'
+token = 'NzcxMDQwNjAzMTYwNzA3MTQz.X5mVhQ.LYrG-HrkYc40de9gKqB0PgogzQc'
+ip = '10.1.1.101'
+port = '12345'
+password = 'password'
