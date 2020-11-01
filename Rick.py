2de11f138dd8550a34a63cb6833db44eabe252bd
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-01 22:47:38 +0100

    Add files via upload

--- Rick.py
+++ Rick.py
@@ -10,8 +10,9 @@ client = commands.Bot(command_prefix = '.', intents=intents, help_command=None)
 
 @client.event
 async def on_ready():
+    member_count = ctx.guild.member_count
     await client.change_presence(status=discord.Status.do_not_disturb,
-    activity=discord.Activity(type=discord.ActivityType.listening, name="to Hydro Applications"))
+    activity=discord.Activity(type=discord.ActivityType.watching, name=f'{member_count} Hydro Members'))
     print('Bot is ready.')
 
 @client.command()
@@ -36,20 +37,10 @@ async def unload_error(ctx, error):
     if isinstance(error, commands.MissingRole):
         await ctx.send("You don't have the permissions to do that!")
 
-@client.command()
-async def iam(ctx, *, role):
-    user = ctx.message.author
-    role = discord.utils.get(ctx.guild.roles, name=f'{role}')
-    if role in user.roles:
-        await ctx.send(f"You already have {role} role.")
-    else:
-        await user.add_roles(role)
-        await ctx.send(f"You've been given {role} role.")
 
 for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-
 
 if __name__ == '__main__':
     import config
