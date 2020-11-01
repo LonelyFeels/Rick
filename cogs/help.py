1c06a4443107e0fc47ec54410feb44db0ae70e39
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-01 22:34:58 +0100

    Add files via upload

--- cogs/help.py
+++ cogs/help.py
@@ -36,10 +36,11 @@ class Help(commands.Cog):
         embedhelp.add_field(name='.whitelist <IGN>', value='Use this to whitelist the member.', inline=False)
         embedhelp.add_field(name='.unwhitelist <IGN>', value='Use this to unwhitelist the member.', inline=False)
         embedhelp.add_field(name='.ban <@USER> <IGN>', value='Use this to ban the member both from Discord and Minecraft server.', inline=False)
-        embedhelp.add_field(name='.unban <IGN> <USER#DISCRIMINATOR>', value='Use this to unban both from Discord and Minecraft server.', inline=False)
+        embedhelp.add_field(name='.unban <IGN> -> <USER#DISCRIMINATOR>', value='Use this to unban the member both from Discord and Minecraft server.', inline=False)
+        embedhelp.add_field(name='.kick <IGN>', value='Use this to kick the member from Discord server.', inline=False)
         embedhelp.add_field(name='.clear <amount=5>', value='Use this to clear messages.', inline=False)
         embedhelp.add_field(name='_ _', value='_ _', inline=False)
-        embedhelp.add_field(name='Categories:', value='Help: Help, Donate, Invite, Dynmap; Status: Status; Roles: Accept, Reaction Roles; RCON: Whitelist, Unwhitelist, Ban, Unban, Close; Suggestions: Suggest; Clear: Clear; Fun:  Hug, 8ball, Waifu', inline=False)
+        embedhelp.add_field(name='Categories:', value='Help: Help, Donate, Invite, Dynmap; Status: Status; Roles: Accept, Reaction Roles; RCON: Whitelist, Unwhitelist, Ban, Unban, Kick, Close; Suggestions: Suggest; Clear: Clear; Fun:  Hug, 8ball, Waifu', inline=False)
 
         await ctx.send(embed=embedhelp)
         
