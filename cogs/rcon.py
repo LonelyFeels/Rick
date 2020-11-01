3485687cd764724bed6a36495ebe807124e458f5
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-01 13:42:16 +0100

    Add files via upload

--- cogs/rcon.py
+++ cogs/rcon.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from minecraft import Server
+import json
+import os
 
 
 class RCON(commands.Cog):
@@ -8,21 +10,23 @@ class RCON(commands.Cog):
     def __init__(self,client):
         self.client = client
 
+
     #Commands
     @commands.command()
     @commands.has_role('Staff')
     async def whitelist(self, ctx, player):
-        if __name__ == '__main__':
-            import config
-            ip = config.ip
-            port = config.port
-            password = config.password
+        if os.path.exists('config.json'):
+            with open('config.json') as file:
+                config = json.load(file)
+        ip = config['IP']
+        port = config['port']
+        password = config['password']
 
-            server = Server(ip, port, password, connect_on_send=True)
-            print(await server.send(f'whitelist add {player}'))
-            await ctx.send(f'Added {player} to the whitelist.')
+        server = Server(ip, port, password, connect_on_send=True)
+        print(await server.send(f'whitelist add {player}'))
+        await ctx.send(f'Added {player} to the whitelist.')
 
-            await server.close()
+        await server.close()
 
     @whitelist.error
     async def whitelist_error(self, player, error):
@@ -34,17 +38,18 @@ class RCON(commands.Cog):
     @commands.command()
     @commands.has_role('Staff')
     async def unwhitelist(self, ctx, player):
-         if __name__ == '__main__':
-            import config
-            ip = '46.4.13.165'
-            port = 25567
-            password = '@LdY%U430G8NVS8sqsJAR$z#m08763'
+        if os.path.exists('config.json'):
+            with open('config.json') as file:
+                config = json.load(file)
+        ip = config['IP']
+        port = config['port']
+        password = config['password']
 
-            server = Server(ip, port, password, connect_on_send=True)
-            print(await server.send(f'whitelist remove {player}'))
-            await ctx.send(f'Removed {player} to the whitelist.')
+        server = Server(ip, port, password, connect_on_send=True)
+        print(await server.send(f'whitelist remove {player}'))
+        await ctx.send(f'Removed {player} to the whitelist.')
 
-            await server.close()
+        await server.close()
     
     @unwhitelist.error
     async def unwhitelist_error(self, player, error):
