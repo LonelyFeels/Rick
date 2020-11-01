ecf3439635d87aa4e7104bcf37bf49be1c29b78c
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-01 12:01:19 +0100

    Add files via upload

--- cogs/rcon.py
+++ cogs/rcon.py
@@ -28,7 +28,7 @@ class Rcon(commands.Cog):
         rcon = RCONClient(HOST, port=PORT)
         if rcon.login('@LdY%U430G8NVS8sqsJAR$z#m08763'):
             rcon.command(f'whitelist add {ctx}')
-            await ctx.say(f'Removed {ctx} from the whitelist.')
+            await ctx.send(f'Removed {ctx} from the whitelist.')
 
 
 def setup(client):
