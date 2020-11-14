024d35cb4892590a5f08ee957236f03bae4adc7a
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-14 23:26:16 +0100

    Add files via upload

--- Rick.py
+++ Rick.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
-client = commands.Bot(command_prefix = '.', intents=intents, help_command=None)
+client = commands.Bot(command_prefix = '!', intents=intents, help_command=None)
 
 
 @client.event
