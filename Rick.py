ffd6fe0e3dca8943dc8f603fa739b853503ab0b6
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-20 15:31:43 +0100

    Add files via upload

--- Rick.py
+++ Rick.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from discord.ext import commands
+import asyncio
 
 
 intents = discord.Intents.default()
@@ -8,13 +9,27 @@ intents.members = True
 client = commands.Bot(command_prefix = '!', intents=intents, help_command=None)
 
 
+async def online_users_task(ctx):
+    while True:
+        online_users = sum(member.status!=discord.Status.offline and not member.bot for member in client.get_all_members())
+        online_channel = client.get_channel(615522198073114625)
+        await online_channel.edit(name = f'Online Users: {online_users}')
+        await asyncio.sleep(60)
+
 @client.event
-async def on_ready():
+async def on_ready(ctx):
     member_count = sum(1 for _ in client.get_all_members())
     await client.change_presence(status=discord.Status.do_not_disturb,
     activity=discord.Activity(type=discord.ActivityType.watching, name=f'{member_count} Hydro Members'))
     print('Bot is ready.')
 
+    client.loop.create_task(online_users_task(ctx))
+
+@client.command()
+@commands.has_role('Staff')
+async def stoponline(ctx):
+    client.loop.stop(online_users_task(ctx))
+    
 @client.event
 async def on_member_join(member: discord.Member):
     total_users = sum(1 for _ in client.get_all_members())
@@ -26,12 +41,6 @@ async def on_member_remove(member: discord.Member):
     total_users = sum(1 for _ in client.get_all_members())
     total_channel = client.get_channel(615522151742701590)
     await total_channel.edit(name = f'Total Users: {total_users}')
-
-@client.event
-async def on_member_update(member: discord.Member):
-    online_users = sum(member.status!=discord.Status.offline and not member.bot for member in client.get_all_members())
-    online_channel = client.get_channel(615522198073114625)
-    await online_channel.edit(name = f'Online Users: {online_users}')
 
 @client.command()
 @commands.has_role('botadmin')
