264e10bf51d6fb886ec06a7662ef9b76cf61a277
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2020-11-22 16:01:09 +0100

    Add files via upload

--- Rick.py
+++ Rick.py
@@ -15,11 +15,14 @@ async def on_ready():
     guild = client.get_guild(494184372258471936)
     online_channel = client.get_channel(615522198073114625)
     total_channel = client.get_channel(615522151742701590)
+    bot_channel = client.get_channel(615522220672024612)
     async def count_users_task():
         while True:
             members = [m.status for m in guild.members]
+            bots = [m.status for m in guild.members if m.bot]
             await online_channel.edit(name = f'Online Users: {members.count(discord.Status.online)+members.count(discord.Status.idle)+members.count(discord.Status.do_not_disturb)}')
             await total_channel.edit(name = f'Total Users: {len(client.users)}')
+            await bot_channel.edit(name = f'Bots: {bots.count(discord.Status.online)+bots.count(discord.Status.idle)+bots.count(discord.Status.do_not_disturb)}')
             await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.watching, name=f'{len(client.users)} Hydro Members'))
             await asyncio.sleep(1200)
     client.loop.create_task(count_users_task())
