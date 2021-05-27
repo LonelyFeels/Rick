d7967f99857e572c0fe2ea5036d6ed16757c1c56
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2021-05-27 17:47:48 +0200

    Add files via upload

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -12,7 +12,7 @@ class Leaderboard(commands.Cog):
     #Commands
     @commands.command()
     @commands.has_any_role('Staff', 'GameMaster')
-    async def lbm(self, ctx, member: discord.Member):
+    async def lbr(self, ctx, member: discord.Member):
         with open('leaderboard.json', 'r', encoding='utf8') as file:
             users = json.load(file)
             if str(member.id) in users:
@@ -23,14 +23,30 @@ class Leaderboard(commands.Cog):
                     users[str(member.id)] = {}
                     users[str(member.id)]['points'] = 0
                     json.dump(users, file, sort_keys=True, indent=4, ensure_ascii=False)
-                    await ctx.send(f'@{member} sucessfully registered into GuildWars.')
+                    await ctx.send(f'@{member} successfully registered into GuildWars.')
 
-    @lbm.error
+    @lbr.error
     async def lbm_error(self, member, error):
         if isinstance(error, commands.MissingRole):
             await member.send('You don\'t have the permissions to do that!')
         if isinstance(error, commands.MissingRequiredArgument):
             await member.send('You have to mention the Member you want to add to the database.')
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lba(self, ctx, member: discord.Member, number:int):
+        with open('leaderboard.json', 'r') as file:
+            users = json.load(file)
+
+        users[str(member.id)]['points'] = users[str(member.id)]['points'] + number
+
+        with open('leaderboard.json', 'w') as file:
+            json.dump(users, file, sort_keys=True, indent=4, ensure_ascii=False)
+
+        with open('leaderboard.json', 'r') as file:
+            users = json.load
+
+        await ctx.send(f'Successfully updated @{member}\'s points to' + {users[str(member.id)]['points']})
 
 
 def setup(client):
