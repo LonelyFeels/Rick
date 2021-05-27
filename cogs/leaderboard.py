9699be220373a5309e4919f872c146ea134177e6
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2021-05-27 13:51:02 +0200

    Add files via upload

---
+++ cogs/leaderboard.py
@@ -0,0 +1,77 @@
+import discord
+from discord.ext import commands
+import json
+
+
+class Leaderboard(commands.Cog):
+
+    def __init__(self,client):
+        self.client = client
+
+
+    #Commands
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lbm(self, ctx, member: discord.Member):
+        with open('leaderboard.json', 'w', encoding='utf8') as file:
+            user = {}
+            user[str(member.id)] = {}
+            user[str(member.id)]['points'] = 0
+            json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
+
+    @lbm.error
+    async def lbm_error(self, member, error):
+        if isinstance(error, commands.MissingRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to mention the Member you want to add to the database.')
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lba(self, ctx, member: discord.Member):
+        try:
+            with open('leaderboard.json', 'r', encoding='utf8') as file:
+                user = json.load(file)
+            with open('leaderboard.json', 'w', encoding='utf8') as file:
+                user[str(member.id)]['points'] = user[str(member.id)]['points'] + ctx
+        except:
+            await ctx.send(f'{member} is not in the database. Let me add them for you.')
+            with open('leaderboard.json', 'w', encoding='utf8') as file:
+                user = {}
+                user[str(member.id)] = {}
+                user[str(member.id)]['points'] = 0
+                json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
+    
+    @lba.error
+    async def lba_error(self, ctx, member, error):
+        if isinstance(error, commands.MissingRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to put down points and mention Member you want to add points.')
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lbs(self, ctx, member: discord.Member):
+        try:
+            with open('leaderboard.json', 'r', encoding='utf8') as file:
+                user = json.load(file)
+            with open('leaderboard.json', 'w', encoding='utf8') as file:
+                user[str(member.id)]['points'] = user[str(member.id)]['points'] - ctx
+        except:
+            await ctx.send(f'{member} is not in the database. Let me add them for you.')
+            with open('leaderboard.json', 'w', encoding='utf8') as file:
+                user = {}
+                user[str(member.id)] = {}
+                user[str(member.id)]['points'] = 0
+                json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
+
+    @lbs.error
+    async def lbs_error(self, ctx, member, error):
+        if isinstance(error, commands.MissingRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to put down points and mention Member you want to subtract points.')
+
+
+def setup(client):
+    client.add_cog(Leaderboard(client))
