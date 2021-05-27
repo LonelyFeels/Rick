de0993cc2168f66c1ba7e55acc74da0b198729e6
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2021-05-27 17:58:40 +0200

    Add files via upload

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -12,7 +12,7 @@ class Leaderboard(commands.Cog):
     #Commands
     @commands.command()
     @commands.has_any_role('Staff', 'GameMaster')
-    async def lbr(self, ctx, member: discord.Member):
+    async def lbregister(self, ctx, member: discord.Member):
         with open('leaderboard.json', 'r', encoding='utf8') as file:
             users = json.load(file)
             if str(member.id) in users:
@@ -25,28 +25,43 @@ class Leaderboard(commands.Cog):
                     json.dump(users, file, sort_keys=True, indent=4, ensure_ascii=False)
                     await ctx.send(f'@{member} successfully registered into GuildWars.')
 
-    @lbr.error
-    async def lbm_error(self, member, error):
+    @lbregister.error
+    async def lbregister_error(self, member, error):
         if isinstance(error, commands.MissingRole):
             await member.send('You don\'t have the permissions to do that!')
         if isinstance(error, commands.MissingRequiredArgument):
-            await member.send('You have to mention the Member you want to add to the database.')
+            await member.send('You have to mention the Member you want to add to the database!')
 
     @commands.command()
     @commands.has_any_role('Staff', 'GameMaster')
-    async def lba(self, ctx, member: discord.Member, number:int):
+    async def lbadd(self, ctx, member: discord.Member, number:int):
         with open('leaderboard.json', 'r') as file:
             users = json.load(file)
+            if str(member.id) in users:
+                users[str(member.id)]['points'] = users[str(member.id)]['points'] + number
 
-        users[str(member.id)]['points'] = users[str(member.id)]['points'] + number
+                with open('leaderboard.json', 'w') as file:
+                    json.dump(users, file, sort_keys=True, indent=4, ensure_ascii=False)
 
-        with open('leaderboard.json', 'w') as file:
-            json.dump(users, file, sort_keys=True, indent=4, ensure_ascii=False)
-
-        with open('leaderboard.json', 'r') as file:
-            users = json.load
-
-        await ctx.send(f'Successfully updated @{member}\'s points to' + {users[str(member.id)]['points']})
+                with open('leaderboard.json', 'r') as file:
+                    users = json.load
+                upoints = users[str(member.id)]['points']
+                await ctx.send(f'Successfully updated @{member}\'s points to {upoints}')
+            else:
+                await ctx.send(f'@{member} is not in the GuildWars database. Let me add them for you.')
+                with open('leaderboard.json', 'w', encoding='utf8') as file:
+                    users = {}
+                    users[str(member.id)] = {}
+                    users[str(member.id)]['points'] = 0
+                    json.dump(users, file, sort_keys=True, indent=4, ensure_ascii=False)
+                    await ctx.send(f'@{member} successfully registered into GuildWars.')
+    
+    @lbadd.error
+    async def lbadd_error(self, member, error):
+        if isinstance(error, commands.MissingRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to mention the Member you want to add points to!')
 
 
 def setup(client):
