eba372f8f3fd49afe35d49bb2161aeb854b57dc2
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2021-05-27 15:57:06 +0200

    Add files via upload

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -13,11 +13,15 @@ class Leaderboard(commands.Cog):
     @commands.command()
     @commands.has_any_role('Staff', 'GameMaster')
     async def lbm(self, ctx, member: discord.Member):
-        with open('leaderboard.json', 'w', encoding='utf8') as file:
-            user = {}
-            user[str(member.id)] = {}
-            user[str(member.id)]['points'] = 0
-            json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
+        file = json.loads('leaderboard.json')
+        if f'{member.id}' in file:
+            member.send('User already registered in database.')
+        else:
+            with open('leaderboard.json', 'w', encoding='utf8') as file:
+                user = {}
+                user[str(member.id)] = {}
+                user[str(member.id)]['points'] = 0
+                json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
 
     @lbm.error
     async def lbm_error(self, member, error):
@@ -25,58 +29,6 @@ class Leaderboard(commands.Cog):
             await member.send('You don\'t have the permissions to do that!')
         if isinstance(error, commands.MissingRequiredArgument):
             await member.send('You have to mention the Member you want to add to the database.')
-
-    @commands.command()
-    @commands.has_any_role('Staff', 'GameMaster')
-    async def lba(self, ctx, member: discord.Member, number:int):
-        try:
-            with open('leaderboard.json', 'r', encoding='utf8') as file:
-                user = json.load(file)
-            with open('leaderboard.json', 'w', encoding='utf8') as file:
-                user[str(member.id)]['points'] = user[str(member.id)]['points'] + number
-                json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
-        except:
-            await member.send(f'{member} is not in the database. Let me add them for you.')
-            with open('leaderboard.json', 'r', encoding='utf8') as file:
-                user = json.load(file)
-            with open('leaderboard.json', 'w', encoding='utf8') as file:
-                user = {}
-                user[str(member.id)] = {}
-                user[str(member.id)]['points'] = 0 + number
-                json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
-    
-    @lba.error
-    async def lba_error(self, member, error):
-        if isinstance(error, commands.MissingRole):
-            await member.send('You don\'t have the permissions to do that!')
-        if isinstance(error, commands.MissingRequiredArgument):
-            await member.send('You have to put down points and mention Member you want to add points.')
-
-    @commands.command()
-    @commands.has_any_role('Staff', 'GameMaster')
-    async def lbs(self, ctx, member: discord.Member, number:int):
-        try:
-            with open('leaderboard.json', 'r', encoding='utf8') as file:
-                user = json.load(file)
-            with open('leaderboard.json', 'w', encoding='utf8') as file:
-                user[str(member.id)]['points'] = user[str(member.id)]['points'] - number
-                json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
-        except:
-            await member.send(f'{member} is not in the database. Let me add them for you.')
-            with open('leaderboard.json', 'r', encoding='utf8') as file:
-                user = json.load(file)
-            with open('leaderboard.json', 'w', encoding='utf8') as file:
-                user = {}
-                user[str(member.id)] = {}
-                user[str(member.id)]['points'] = 0 - number
-                json.dump(user, file, sort_keys=True, indent=4, ensure_ascii=False)
-
-    @lbs.error
-    async def lbs_error(self, member, error):
-        if isinstance(error, commands.MissingRole):
-            await member.send('You don\'t have the permissions to do that!')
-        if isinstance(error, commands.MissingRequiredArgument):
-            await member.send('You have to put down points and mention Member you want to subtract points.')
 
 
 def setup(client):
