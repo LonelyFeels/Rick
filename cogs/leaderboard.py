2bde81b2cf2decb622994cbc935976a94e657ce3
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2021-05-29 13:11:48 +0200

    Add files via upload

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -100,6 +100,70 @@ class Leaderboard(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await member.send('You have to mention the Member you want to add points to and put down the number of points!')
 
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lbsubtract(self, ctx, member: discord.Member, number:int):
+        db = mysql.connector.connect(
+            host = mysqlcredentials.host,
+            port = mysqlcredentials.port,
+            user = mysqlcredentials.user,
+            password = mysqlcredentials.password,
+            database = mysqlcredentials.database
+        )
+        mycursor = db.cursor()
+
+        mycursor.execute(f"SELECT * FROM User WHERE id={str(member.id)}")
+        data = mycursor.fetchall()
+        if len(data)==0:
+            await ctx.send(f'@{member} is not in the GuildWars database. Let me add them for you.')
+            mycursor.execute("INSERT INTO User (id, points) VALUES (%s, %s)", (f"{member.id}", 0-number))
+            db.commit()
+            await ctx.send(f'@{member} successfully registered into GuildWars with starting points of -{number}.')
+        else:
+            mycursor.execute(f"SELECT points FROM User WHERE id={str(member.id)}")
+            points = mycursor.fetchall()[0][0]
+            mycursor.execute(f"UPDATE User SET points={int(points)-number} WHERE id={str(member.id)}")
+            db.commit()
+            await ctx.send(f'Successfully updated @{member}\'s points to {int(points)-number}.')
+
+    @lbsubtract.error
+    async def lbsubtract_error(self, member, error):
+        if isinstance(error, commands.MissingRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to mention the Member you want to subtract points from and put down the number of points!')
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lbreset(self, ctx, member: discord.Member):
+        db = mysql.connector.connect(
+            host = mysqlcredentials.host,
+            port = mysqlcredentials.port,
+            user = mysqlcredentials.user,
+            password = mysqlcredentials.password,
+            database = mysqlcredentials.database
+        )
+        mycursor = db.cursor()
+
+        mycursor.execute(f"SELECT * FROM User WHERE id={str(member.id)}")
+        data = mycursor.fetchall()
+        if len(data)==0:
+            await ctx.send(f'@{member} is not in the GuildWars database. Let me add them for you.')
+            mycursor.execute("INSERT INTO User (id, points) VALUES (%s, %s)", (f"{member.id}", 0))
+            db.commit()
+            await ctx.send(f'@{member} successfully registered into GuildWars with starting points of 0.')
+        else:
+            mycursor.execute(f"UPDATE User SET points=0 WHERE id={str(member.id)}")
+            db.commit()
+            await ctx.send(f'Successfully updated @{member}\'s points to 0.')
+    
+    @lbreset.error
+    async def lbreset_error(self, member, error):
+        if isinstance(error, commands.MissingRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to mention the Member you want to reset points to!')
+
 
 def setup(client):
     client.add_cog(Leaderboard(client))
