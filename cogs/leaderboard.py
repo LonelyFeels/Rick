a682048b85ae88b546edacee98fd56a26dd72982
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2021-05-29 11:19:54 +0200

    Add files via upload

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -28,8 +28,36 @@ class Leaderboard(commands.Cog):
         if len(data)==0:
             mycursor.execute("INSERT INTO User (id, points) VALUES (%s, %s)", (f"{member.id}", 0))
             db.commit()
+            await ctx.send(f'@{member} successfully registered into GuildWars.')
         else:
             await ctx.send('Member is already registered in GuildWars database!')
+
+    @lbregister.error
+    async def lbregister_error(self, member, error):
+        if isinstance(error, commands.MissingRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to mention the Member you want to add to the database!')
+    
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lbunregister(self, ctx, member: discord.Member):
+        db = mysql.connector.connect(
+            host = mysqlcredentials.host,
+            port = mysqlcredentials.port,
+            user = mysqlcredentials.user,
+            password = mysqlcredentials.password,
+            database = mysqlcredentials.database
+        )
+        mycursor = db.cursor()
+
+        mycursor.execute(f"SELECT * FROM User WHERE id={str(member.id)}")
+        data = mycursor.fetchall()
+        if len(data)==0:
+            await ctx.send('Member hasn\'t been registered to Guild Wars database yet!')
+        else:
+            mycursor.execute(f"DELETE FROM User WHERE id={str(member.id)}")
+            await ctx.send(f'@{member} successfully unregistered from GuildWars.')
 
 
 def setup(client):
