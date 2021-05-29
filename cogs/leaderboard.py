f1a8dbd77d3de7d62e3eb6a75eca9c13cb964e5c
LonelyFeels <73558135+LonelyFeels@users.noreply.github.com>
2021-05-29 12:45:38 +0200

    Add files via upload

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -34,7 +34,7 @@ class Leaderboard(commands.Cog):
 
     @lbregister.error
     async def lbregister_error(self, member, error):
-        if isinstance(error, commands.MissingRole):
+        if isinstance(error, commands.MissingAnyRole):
             await member.send('You don\'t have the permissions to do that!')
         if isinstance(error, commands.MissingRequiredArgument):
             await member.send('You have to mention the Member you want to add to the database!')
@@ -59,6 +59,46 @@ class Leaderboard(commands.Cog):
             mycursor.execute(f"DELETE FROM User WHERE id={str(member.id)}")
             db.commit()
             await ctx.send(f'@{member} successfully unregistered from GuildWars.')
+
+    @lbunregister.error
+    async def lbunregister_error(self, member, error):
+        if isinstance(error, commands.MissingAnyRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to mention the Member you want to remove from the database!')
+
+    @commands.command()
+    @commands.has_any_role('Staff', 'GameMaster')
+    async def lbadd(self, ctx, member: discord.Member, number):
+        db = mysql.connector.connect(
+            host = mysqlcredentials.host,
+            port = mysqlcredentials.port,
+            user = mysqlcredentials.user,
+            password = mysqlcredentials.password,
+            database = mysqlcredentials.database
+        )
+        mycursor = db.cursor()
+
+        mycursor.execute(f"SELECT * FROM User WHERE id={str(member.id)}")
+        data = mycursor.fetchall()
+        if len(data)==0:
+            await ctx.send(f'@{member} is not in the GuildWars database. Let me add them for you.')
+            mycursor.execute("INSERT INTO User (id, points) VALUES (%s, %s)", (f"{member.id}", number))
+            db.commit()
+            await ctx.send(f'@{member} successfully registered into GuildWars with starting points of {number}.')
+        else:
+            mycursor.execute(f"SELECT points FROM User WHERE id={str(member.id)}")
+            points = mycursor.fetchall()
+            mycursor.execute(f"UPDATE User SET points={points+number} WHERE id={str(member.id)}")
+            db.commit()
+            await ctx.send(f'Successfully updated @{member}\'s points to {points+number}.')
+
+    @lbadd.error
+    async def lbadd_error(self, member, error):
+        if isinstance(error, commands.MissingAnyRole):
+            await member.send('You don\'t have the permissions to do that!')
+        if isinstance(error, commands.MissingRequiredArgument):
+            await member.send('You have to mention the Member you want to add points to and put down the number of points!')
 
 
 def setup(client):
