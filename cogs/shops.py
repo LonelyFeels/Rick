9906d9ec40bc5fb79bbb8519a90b84cf6d913e56
StephenKairos <stephenkairos.reyes@sjsu.edu>
2021-06-21 21:32:54 -0700

    apostrophes suck

--- cogs/shops.py
+++ cogs/shops.py
@@ -56,8 +56,7 @@ class Shops(commands.Cog):
 
         print(f"Start command: storeedit({str(storename)}, {str(item)}, {str(quantity)}, {str(price)}, {str(description)})")
 
-        mycursor.execute(f"SELECT * FROM Store_Directory WHERE UserID='{str(owner.id)}'")
-        data = []
+        mycursor.execute(f"SELECT * FROM Store_Directory WHERE UserID={str(owner.id)} AND StoreName='{str(storename)}'")
         data = mycursor.fetchall()
         print("test")
         print("Getting Data: " + data)
